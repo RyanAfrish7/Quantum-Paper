692394d4c62e7ff8d1472e6c71c4edfff7799213
RyanAfrish7 <ryanafrish7@gmail.com>
2015-06-17 14:14:48 +0530

    Implemented some core elements
    Things getting readied

--- design.js
+++ design.js
@@ -6,14 +6,27 @@ $(document).ready(function(){
 
 	$(document).on("scroll", function(e) {
 
+		$("#P2").text($(document).scrollTop());
 		if ($(document).scrollTop() > 152){
+
 			headerX.removeClass("large");
 			headerX.addClass("small");
+			headerX.removeClass("ontransition");
+		} else if($(document).scrollTop()>95){
+			if(headerX.hasClass("small"))
+			{
+				headerX.removeClass("small");
+				headerX.addClass("large");
+			}
+			headerX.addClass("ontransition");
 		} else {
+			headerX.removeClass("ontransition");
 			headerX.removeClass("small");
 			headerX.addClass("large");
 		}
 
 	});
+
+
 
 });
